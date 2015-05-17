9fb4a3099e59ceca04610a19170905eb853578eb
Kuod <louismougin54@gmail.com>
2015-05-17 02:14:05 +0200

    ajout fonction clavier

--- optimisation/modele/Clavier.java
+++ optimisation/modele/Clavier.java
@@ -1,5 +1,6 @@
 package optimisation.modele;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Clavier {
@@ -20,7 +21,35 @@ public class Clavier {
 		}
 	}
 	
+	//Fonction pour générer un clavier avec un nombre de lettre de départ
+	//Utile ou non, va savoir
+	public Clavier(int nbLettre) {
+		clavier = new char[4][10];
+		int i,j,k;
+		int taille = nbLettre;
+		Random r = new Random();
+		if(nbLettre>26) taille = 26;
+		else if(nbLettre<=0) taille = 1;
+		ArrayList<Character> lettreAlea = new ArrayList<Character>(taille);
+		for(i = 0; i<taille;i++) {
+			j = r.nextInt(26);
+			if(!lettreAlea.contains(lettre[j])) lettreAlea.add(lettre[j]);
+			else i--;
+		}
+		
+		for(i = 0;i<taille;i++) {
+			j = r.nextInt(4);
+			k = r.nextInt(10);
+			if(clavier[j][k]<65) clavier[j][k] = lettreAlea.get(i);
+			else i--;
+		}
+	}
+	
 	public char[][] getClavier() {
 		return clavier;
+	}
+	
+	public boolean estVide(int ligne, int colonne) {
+		return clavier[ligne][colonne]<65;
 	}
 }
