f934e253fcddd7358adaa6e6cdf82208d46ccef2
AnthonM <anthony290991@hotmail.com>
2015-05-22 00:02:45 +0200

    Update VueAlgorithmeRecuitSimule.java

--- optimisation/vue/recuit/VueAlgorithmeRecuitSimule.java
+++ optimisation/vue/recuit/VueAlgorithmeRecuitSimule.java
@@ -1,48 +1,47 @@
 package optimisation.vue.recuit;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
-import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-import optimisation.modele.Clavier;
 import optimisation.modele.Modele;
 import optimisation.modele.algorithme.AlgorithmeRecuitSimule;
 import optimisation.vue.VueAlgorithme;
 import optimisation.vue.VueClavier;
+import optimisation.ecouteur.EcouterBoutonLance;
 
+@SuppressWarnings("serial")
 public class VueAlgorithmeRecuitSimule extends VueAlgorithme{
 
 	protected GridLayout grid;
-	protected VueClavier[][] tabClavier;
+	protected VueClavier vc;
+	protected JButton jb;
+	protected JPanel jp;
 	
-	//Ton algo te servira de Modele
 	protected AlgorithmeRecuitSimule ars;
 	
 	public VueAlgorithmeRecuitSimule(Modele m) {
 		super(m);
 		
-		//Tu stockes ton "modele"
-		//A voir si on passe pas en modele Observer/Observable
-		//Avec l'algo le Modèle (Observable) et la vue, l'observer
 		this.ars = (AlgorithmeRecuitSimule)m.getAlgo();
-		grid = new GridLayout(2,4);
-		tabClavier = new VueClavier[2][4];
-		grid.setHgap(7);
-		grid.setVgap(15);
-		this.setBackground(Color.BLACK);
-		this.setLayout(grid);
-		for(int i = 0; i<tabClavier.length;i++) {
-			for(int j = 0; j<tabClavier[i].length;j++) {
-				tabClavier[i][j] = new VueClavier(new Clavier());
-				this.add(tabClavier[i][j]);
-			}
-		}
+		vc = new VueClavier(ars.getClavier());
+		jb = new JButton("Lance");
+		jp = new JPanel();
+		
+		jp.add(jb);
+		
+		jb.addActionListener(new EcouterBoutonLance(ars,this));
+		this.add(vc,BorderLayout.CENTER);
+		this.add(jp,BorderLayout.SOUTH);
+		
+		
 	}
 	
 	@Override
 	public void maj() {
-	
+		vc.majClavier(ars.getTampon());
 	}
 
 }
