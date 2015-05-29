dafbc6b2f5bcf76f83f8f0451c4a8104f294a95d
AnthonM <anthony290991@hotmail.com>
2015-05-29 15:21:51 +0200

    Update EcouterBoutonLance.java

--- optimisation/ecouteur/EcouterBoutonLance.java
+++ optimisation/ecouteur/EcouterBoutonLance.java
@@ -8,7 +8,6 @@ import javax.swing.JOptionPane;
 
 import optimisation.modele.algorithme.Algorithme;
 import optimisation.modele.algorithme.AlgorithmeRecuitSimule;
-import optimisation.modele.algorithme.AlgorithmeTabou;
 import optimisation.vue.VueAlgorithme;
 import optimisation.vue.recuit.VueAlgorithmeRecuitSimule;
 
@@ -25,57 +24,21 @@ public class EcouterBoutonLance implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		/*
-		int temperature=0;
-		double lambda=0;
-		boolean b = true,c = true;
-		while(b)
+		
+		if(a.getNom()=="Algorithme Recuit Simule")
 		{
-			String t; 
-			try
-			{
-				 t = JOptionPane.showInputDialog(null, "Veuillez rentrer une température", "Temperature", JOptionPane.QUESTION_MESSAGE);
-				temperature = Integer.parseInt(t);
-				b=false;
-			}
-		    catch(NumberFormatException e)
-			{
-		    	JOptionPane.showMessageDialog(null, "Veuillez rentrer une température exact", "Erreur", JOptionPane.ERROR_MESSAGE);
-		    }
-		    
+			a.getAlgorithmeRecuitSimule().recuitSimule();
+			va.maj();
+		}
+		else if(a.getNom()=="Recherche Tabou")
+		{
+			
+		}
+		else
+		{
+			System.out.println("probleme");
 		}
 		
-		while(c)
-		{
-			String t,s=""; 
-			try
-			{
-				 t = JOptionPane.showInputDialog(null, "Veuillez le nombre lambda(compris entre 0.99 et 1)", "Lambda", JOptionPane.QUESTION_MESSAGE);
-				lambda = Double.parseDouble(t);
-				if(lambda>=1)
-				{
-					JOptionPane.showMessageDialog(null, "Ceci est supérieur à 1 ou égal", "Erreur", JOptionPane.ERROR_MESSAGE);
-				}
-				else
-				{
-					c=false;
-				}
-		
-			}
-		    catch(NumberFormatException e)
-			{
-		    	JOptionPane.showMessageDialog(null, "Veuillez rentrer un lambda exact\n"+s, "Erreur", JOptionPane.ERROR_MESSAGE);
-		    }
-		    
-		}
-	  
-		ars.setK(lambda);
-	        ars.setTemperature(temperature);
-		ars.recuitSimule();
-		
-		vc.maj();
-		*/
-		va.maj();
 	}
 	
 	
